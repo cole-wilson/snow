9f89a8d1d39e9078cbdaf60069c511900c05d3b6
Cole Wilson <cole@colewilson.xyz>
2023-07-11 19:15:25 -0700

    first commit

---
+++ snow.js
@@ -0,0 +1,51 @@
+let canvas = document.createElement("canvas");
+let ctx = canvas.getContext("2d")
+canvas.style.zIndex = 999*999;
+setup()
+document.body.appendChild(canvas);
+
+var tick = 0;
+var flakes = [];
+var piles = {};
+
+
+function draw() {
+	if (flakes.length > 2000) {location.reload()}
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	tick++;
+	for (var i=0;i<flakes.length;i++) {
+		let flake = flakes[i]
+		if (!flake.done) {
+			flake.h += flake.v
+			flake.w = flake.ow + 10*Math.sin((tick+i) * Math.PI/180)
+		}
+		if (flake.h - canvas.height > canvas.height + 10) {
+			piles[Math.round(flake.w)] = piles[Math.round(flake.w)]+1 | 0
+			flake.h = canvas.height + canvas.height + 5 - 3*piles[Math.round(flake.w)]
+			flake.done = true
+			newflake()
+		}
+		ctx.fillText("*", flake.w, flake.h - canvas.height)
+	}
+	requestAnimationFrame(draw)
+}
+
+function newflake() {
+	flakes.push({
+		ow:Math.round(canvas.width*Math.random()),
+		h:canvas.height*Math.random(),
+		v:1+ Math.random(),
+		done: false
+	})
+}
+
+for (var i=0;i<1000;i++) newflake()
+
+requestAnimationFrame(draw)
+function setup() {
+	canvas.width = window.outerWidth;
+	canvas.height = window.innerHeight;
+	ctx.font = "20px monospace"
+	ctx.fillStyle = "white";
+}
+window.onresize = setup
